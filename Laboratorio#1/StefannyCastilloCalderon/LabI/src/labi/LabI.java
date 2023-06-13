b95317f79df724dc09a5a3ec96d71e3f3ef1845c
“Stefanny <stefanny2003alexandra@gmail.com>
2023-06-12 22:00:29 -0600

    Actualización Números Primos
    Actualizando el código para realizar la entrega final

--- Laboratorio#1/StefannyCastilloCalderon/LabI/src/labi/LabI.java
+++ Laboratorio#1/StefannyCastilloCalderon/LabI/src/labi/LabI.java
@@ -9,13 +9,24 @@ package labi;
  * @author Stefanny Castillo
  */
 public class LabI {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.print("Hola Mundo");
-        // TODO code application logic here
+    public static void main(String[] args)
+  {
+   	NumeroPrimo(11);
+    NumeroPrimo(12);
+  }
+  
+  public static void NumeroPrimo(int num) {
+    boolean primo = true; 
+    for(int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            primo = false;
+            break;
+        }
     }
-    
+    if (primo)
+        System.out.println("El número es primo.");
+    else
+        System.out.println("El número no es primo.");
+    }
+
 }
