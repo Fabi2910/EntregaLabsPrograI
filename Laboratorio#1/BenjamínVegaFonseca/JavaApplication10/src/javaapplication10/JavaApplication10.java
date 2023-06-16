71736a6a7343a22e4feaff17bd92da4146ad900f
Benjamín Vega Fonseca <benjavefo@gmail.com>
2023-06-15 19:17:52 -0600

    Prueba1
    Solo Prueba1

--- Laboratorio#1/BenjamínVegaFonseca/JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ Laboratorio#1/BenjamínVegaFonseca/JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -14,7 +14,7 @@ public class JavaApplication10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hola Mundo");
+        System.out.print("Hola Mundos");
         // TODO code application logic here
     }
     
