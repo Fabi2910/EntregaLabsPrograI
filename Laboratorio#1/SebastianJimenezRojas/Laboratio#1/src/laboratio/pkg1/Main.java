5117329bdc2850c58e1a135189d2e035752077b7
Sebastian Jimenez Rojas <sebasjimenez099@gmail.com>
2023-06-19 16:34:48 -0600

    Entrega de laboratorio 1 Sebastian
    Profe le realizo la entrega del laboratorio 1

---
+++ Laboratorio#1/SebastianJimenezRojas/Laboratio#1/src/laboratio/pkg1/Main.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratio.pkg1;
+
+/**
+ *
+ * @author 50683
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el límite hasta donde desea evaluar: ");
+        int limite = sc.nextInt();
+        for (int i = 2; i <= limite; i++) {
+            if (esPrimo(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean esPrimo(int numero) {
+        // El 0, 1 y 4 no son primos
+        if (numero == 0 || numero == 1 || numero == 4) {
+            return false;
+        }
+        for (int x = 2; x < numero / 2; x++) {
+            // Si es divisible por cualquiera de estos números, no es primo
+            if (numero % x == 0) {
+                return false;
+            }
+        }
+        // Si no se cumple ninguna de las condiciones anteriores, el número es primo
+        return true;
+    }
+
+}
