16455d2f1491e6a434e2e8f2249777fcf18c5da2
Xavierxr <Saymonaraya49@gmail.com>
2023-06-19 21:20:47 -0600

    adjunto el laboratoio 2
    adjunto el laboratoio 2

---
+++ Laboratorio#2/SaymonXavierArayaGarro/PantalladeProvedores/src/Empleado.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author xavie
+ */
+public class Empleado {
+    
+    
+    private int identificacion; 
+    
+    private String nombre;
+    
+    private String apellido1;
+    
+    private char apellido2; 
+    
+    private double puestoEmpelado;
+    
+    private void darsaludo(){
+        
+    }
+            
+    
+     }
+    
+    
+
