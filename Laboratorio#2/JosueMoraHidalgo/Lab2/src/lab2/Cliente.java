cd2b8fe38dc78887d48debc1587adda158561e5a
JosuM0 <josuemh924@gmail.com>
2023-06-19 23:26:39 -0600

    Entrega lab2 Josue Mora Hidalgo
    Se realiza entrega del laboratorio 2

---
+++ Laboratorio#2/JosueMoraHidalgo/Lab2/src/lab2/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2;
+
+/**
+ *
+ * @author Dell
+ */
+public class Cliente {
+    
+    private int identificacion;
+    
+    private String nombre;
+    
+    private String apellido1;
+    
+    private char apellido2;
+    
+}
