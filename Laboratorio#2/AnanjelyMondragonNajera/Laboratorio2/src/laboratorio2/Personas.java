66564c22948502764c9ba403909982fdacb6953f
Ana0117 <mondragonananjely@gmail.com>
2023-06-19 22:13:48 -0600

    Laboratorio#2
    se sube

---
+++ Laboratorio#2/AnanjelyMondragonNajera/Laboratorio2/src/laboratorio2/Personas.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio2;
+
+/**
+ *
+ * @author Ananjely Mondragon
+ */
+public class Personas {
+    
+    public int identificacion;
+    public String nombre;
+    public String apellido1;
+    public char apellido2;
+    public double puestoEmpleado;
+    
+    public Personas(int identificacion, String nombre, String apellido1, char apellido2, double puestoEmpleado) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.puestoEmpleado = puestoEmpleado;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
