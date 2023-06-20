b13a167ec1d52e2494373d44f7489d410f3bf0b9
Aarón Zúñiga N <90061631+AaronZN07@users.noreply.github.com>
2023-06-19 21:22:08 -0600

    Entrega laboratorio 2 Aarón Zúñiga

---
+++ Laboratorio#2/AaronZunigaNunez/Laboratorio2Proveedores/src/main/Proveedor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author aaron
+ */
+public class Proveedor {
+    
+    private int codigo;
+    private int identificacion;
+    private String nombre;
+    private String telefono;
+    private String direccion;
+    private boolean esNacional;
+    
+    public void crear(){
+        // Código crear proveedor
+    }
+
+    public void eliminar(){
+        // Código eliminar proveedor
+    }
+    
+    public void actualizar(){
+        // Código actualizar proveedor
+    }
+    
+}
