c9516d34e616246497b99d7961bf8e209b87d703
“[laura <sojocastillo2023@gmail.com>
2023-06-23 02:24:55 -0600

    entregalab2
    entrega laboratorio 2

---
+++ Laboratorio#2/LauraSojoCastillo/Proveedor/src/javaapplication3/Empleado.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author sojoc
+ */
+public class Empleado {
+    private int identificacion;
+    
+    private String nombre;
+    
+    private String apellido1;
+    
+    private String apellido2;
+    
+    private double puestoEmpleado;
+    
+    public void darSaludo(){
+       
+    }
+    
+         
+}
