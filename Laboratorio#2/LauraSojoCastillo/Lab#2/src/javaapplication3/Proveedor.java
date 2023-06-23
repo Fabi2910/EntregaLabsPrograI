ccabc92d639f27810143cbad30b54cf18dd2ed00
“[laura <sojocastillo2023@gmail.com>
2023-06-23 02:37:58 -0600

    entrega lab 2

---
+++ Laboratorio#2/LauraSojoCastillo/Lab#2/src/javaapplication3/Proveedor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author sojoc
+ */
+public class Proveedor {
+    private int identificacion;
+    
+    private String nombre;
+    
+    private String apellido1;
+    
+    private char apellido2;
+    
+    private String proveedor;
+    
+}
