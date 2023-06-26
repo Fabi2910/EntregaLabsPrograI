768f008358e7a589fd54f971e1941b9b4a831251
moracald <135674325+moracald@users.noreply.github.com>
2023-06-25 19:04:02 -0600

    Entrega lab2 CarlosMoraCalderon

---
+++ Laboratorio#2/CarlosMoraCalderon/ConceptoHerencia/src/conceptoherencia/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conceptoherencia;
+
+/**
+ *
+ * @author andre
+ */
+public class Cliente {
+    
+    private int identificacion;
+    
+    private String nombre;
+    
+    private String apellido1;
+    
+    private char apellido2;
+    
+    
+}
