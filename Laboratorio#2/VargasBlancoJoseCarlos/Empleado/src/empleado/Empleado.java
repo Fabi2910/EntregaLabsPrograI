15ee6dd18bba9dd5bc7b4ddcf0eb959899c7dd76
“[vargas184b]” <vargas184b@gmail.com>
2023-06-25 20:26:52 -0600

    Lab 2 JoseVargasBlanco
    Buenas noches, hago entrega de mi laboratorio

---
+++ Laboratorio#2/VargasBlancoJoseCarlos/Empleado/src/empleado/Empleado.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package empleado;
+
+/**
+ *
+ * @author Jose
+ */
+public class Empleado {
+
+    public static void main(String[] args) {
+        
+    }
+       private int identificacion;
+       private string nombre;
+       private string apellido1;
+       private char apellido2;
+       private double puestoEmpelado;
+       
+       public void darSaludo() {
+//Esta es la programacion del metodo darSaludo()
+ }
+       
+ }
+
+
+
+
+   
+       
+               
+        
+  
+
+
+                
+   
+    
